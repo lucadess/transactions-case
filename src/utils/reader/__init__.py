d3551aaf29d1fb6f1c7e09f3d38523a82aa3da16
lucadess <lucadessauvagie@gmail.com>
2026-09-18 14:08:54 +0200

    remove csv reader

--- src/utils/reader/__init__.py
+++ src/utils/reader/__init__.py
@@ -1,4 +1,4 @@
 from .factory import get_reader
-from .readers import CSVReader, JSONReader, Reader
+from .readers import JSONReader, Reader
 
-__all__ = ["get_reader", "CSVReader", "JSONReader", "Reader"]
+__all__ = ["get_reader", "JSONReader", "Reader"]
